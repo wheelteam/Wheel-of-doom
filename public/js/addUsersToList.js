59831df6232e00c6a2215ecb9c0cac777b35a4c0
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-06-13 15:29:09 +0200

    capitalize user and add it to list

--- public/js/addUsersToList.js
+++ public/js/addUsersToList.js
@@ -1,8 +1,26 @@
+addUsersToList('SUSANA');
+addUsersToList('SILVIA');
+addUsersToList('sergio');
+
 function addUsersToList(user){
+    user = capitalizeUser(user);
     const userList = document.getElementById("users");
-    userList.appendChild(addUserModule());
+    userList.innerHTML = addUserModule(user);
 }
 
 function addUserModule(user){
-    const module = "<li><input type='text'><button type='submit'></button></li>";
+    const module = "<li>\
+                        <input type='text' placeholder='"+ user +"'>\
+                        <button id='removeUserButton' type='submit'>Eliminar</button>\
+                    </li>";
+    return module;
+}
+
+function capitalizeUser(user) {
+    user = user.toLowerCase();
+    const firstLetter = user[0];
+    const upperCase = firstLetter.toUpperCase();
+    user.replace(user[0], upperCase);
+
+    return user;
 }
