2477434daf0f77bc7ff13a1b5699505dce286064
Sergio-Fernandez-Dev <sr.sergiofernandezfernandez@gmail.com>
2022-06-14 13:59:00 +0200

    fixing overwriting users

--- public/js/addUsersToList.js
+++ public/js/addUsersToList.js
@@ -5,14 +5,14 @@ addUsersToList('sergio');
 function addUsersToList(user){
     user = capitalizeUser(user);
     const userList = document.getElementById("users");
-    userList.innerHTML = addUserModule(user);
+    userList.innerHTML += addUserModule(user);
 }
 
 function addUserModule(user){
-    const module = "<li>\
-                        <input type='text' placeholder='"+ user +"'>\
-                        <button id='removeUserButton' type='submit'>Eliminar</button>\
-                    </li>";
+    const module = `<li>
+                        <input type='text' placeholder='${user}'>
+                        <button id='removeUserButton' type='submit'>x</button>
+                    </li>`;
     return module;
 }
 
@@ -20,7 +20,7 @@ function capitalizeUser(user) {
     user = user.toLowerCase();
     const firstLetter = user[0];
     const upperCase = firstLetter.toUpperCase();
-    user.replace(user[0], upperCase);
+    user.replace(firstLetter, upperCase);
 
     return user;
 }
