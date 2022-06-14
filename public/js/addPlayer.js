61f371da6cc16d7ab5f890ade89cc0cc991022e9
Miguel <miguelsalvador.rguez@gmail.com>
2022-06-14 15:21:37 +0200

    Añadida funcion addPlayer

--- public/js/addPlayer.js
+++ public/js/addPlayer.js
@@ -1,1 +1,14 @@
-let player = new Array ();
+const btn = document.getElementById('addPlayer');
+
+btn.addEventListener('click', function handleClick(event) {
+  
+  event.preventDefault();
+
+  const firstNameInput = document.getElementById('writeNewPlayer').value;
+  
+  addUserToList(firstNameInput);
+
+  
+  firstNameInput.value = '';
+});
+
